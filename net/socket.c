d0ebf5c7dc65f2d7cc8ad7f962e19cb6dcca8ced
Florent Revest <revest@chromium.org>
2020-12-04 12:36:04 +0100

    UPSTREAM: net: Remove the err argument from sock_from_file
    Currently, the sock_from_file prototype takes an "err" pointer that is
    either not set or set to -ENOTSOCK IFF the returned socket is NULL. This
    makes the error redundant and it is ignored by a few callers.
    
    This patch simplifies the API by letting callers deduce the error based
    on whether the returned socket is NULL or not.
    
    Suggested-by: Al Viro <viro@zeniv.linux.org.uk>
    Change-Id: Ifd7fb27dae6eddcd1cc09410ac603ce6be09394b
    Signed-off-by: Florent Revest <revest@google.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Reviewed-by: KP Singh <kpsingh@google.com>
    Link: https://lore.kernel.org/bpf/20201204113609.1850150-1-revest@google.com

--- net/socket.c
+++ net/socket.c
@@ -451,17 +451,15 @@ static int sock_map_fd(struct socket *sock, int flags)
 /**
  *	sock_from_file - Return the &socket bounded to @file.
  *	@file: file
- *	@err: pointer to an error code return
  *
- *	On failure returns %NULL and assigns -ENOTSOCK to @err.
+ *	On failure returns %NULL.
  */
 
-struct socket *sock_from_file(struct file *file, int *err)
+struct socket *sock_from_file(struct file *file)
 {
 	if (file->f_op == &socket_file_ops)
 		return file->private_data;	/* set in sock_map_fd */
 
-	*err = -ENOTSOCK;
 	return NULL;
 }
 EXPORT_SYMBOL(sock_from_file);
@@ -490,9 +488,11 @@ struct socket *sockfd_lookup(int fd, int *err)
 		return NULL;
 	}
 
-	sock = sock_from_file(file, err);
-	if (!sock)
+	sock = sock_from_file(file);
+	if (!sock) {
+		*err = -ENOTSOCK;
 		fput(file);
+	}
 	return sock;
 }
 EXPORT_SYMBOL(sockfd_lookup);
@@ -504,11 +504,12 @@ static struct socket *sockfd_lookup_light(int fd, int *err, int *fput_needed)
 
 	*err = -EBADF;
 	if (f.file) {
-		sock = sock_from_file(f.file, err);
+		sock = sock_from_file(f.file);
 		if (likely(sock)) {
 			*fput_needed = f.flags & FDPUT_FPUT;
 			return sock;
 		}
+		*err = -ENOTSOCK;
 		fdput(f);
 	}
 	return NULL;
