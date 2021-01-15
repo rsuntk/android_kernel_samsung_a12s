53dd2d18f876ba972e9960f917ccce5daf324f6c
Stanislav Fomichev <sdf@google.com>
2021-01-15 08:34:59 -0800

    UPSTREAM: bpf: Remove extra lock_sock for TCP_ZEROCOPY_RECEIVE
    Add custom implementation of getsockopt hook for TCP_ZEROCOPY_RECEIVE.
    We skip generic hooks for TCP_ZEROCOPY_RECEIVE and have a custom
    call in do_tcp_getsockopt using the on-stack data. This removes
    3% overhead for locking/unlocking the socket.
    
    Without this patch:
         3.38%     0.07%  tcp_mmap  [kernel.kallsyms]  [k] __cgroup_bpf_run_filter_getsockopt
                |
                 --3.30%--__cgroup_bpf_run_filter_getsockopt
                           |
                            --0.81%--__kmalloc
    
    With the patch applied:
         0.52%     0.12%  tcp_mmap  [kernel.kallsyms]  [k] __cgroup_bpf_run_filter_getsockopt_kern
    
    Note, exporting uapi/tcp.h requires removing netinet/tcp.h
    from test_progs.h because those headers have confliciting
    definitions.
    
    Change-Id: I90ac0ca5489f29033358f3d438ef9f24d9f75c37
    Signed-off-by: Stanislav Fomichev <sdf@google.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Martin KaFai Lau <kafai@fb.com>
    Link: https://lore.kernel.org/bpf/20210115163501.805133-2-sdf@google.com
    Signed-off-by: rssu. <rissu.ntk@gmail.com>

--- include/net/sock.h
+++ include/net/sock.h
@@ -1193,6 +1193,8 @@ struct proto {
 
 	int			(*backlog_rcv) (struct sock *sk,
 						struct sk_buff *skb);
+	bool			(*bpf_bypass_getsockopt)(int level,
+							 int optname);
 
 	void		(*release_cb)(struct sock *sk);
 
