92876f72bf6c03aa5ae07f226dcde75ccfade041
rsuntk <farisjihadih@outlook.com>
2024-04-01 22:15:17 +0700

    [beta] ci: add build multiple kernel

---
+++ ci_script/build_multiple.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env bash
+
+#
+# Rissu Project (C) 2024
+#
+
+bash $(pwd)/build_ksu_stable_permissive.sh
+rm -rR $(pwd)/out -f
+
+bash $(pwd)/build_ksu_stable_enforcing.sh
+rm -rR $(pwd)/out -f
+
+bash $(pwd)/build_ksu_dev_permissive.sh
+rm -rR $(pwd)/out -f
+
+bash $(pwd)/build_ksu_dev_enforcing.sh
+rm -rR $(pwd)/out -f
+
+bash $(pwd)/build_kernel_permissive.sh
+rm -rR $(pwd)/out -f
+
+bash $(pwd)/build_kernel_enforcing.sh
+rm -rR $(pwd)/out -f
