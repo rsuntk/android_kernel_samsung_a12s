8c929348cb20397cdf3bfc54ed481b9ce8676a65
rsuntk <rissu.ntk@gmail.com>
2024-07-13 01:48:24 +0700

    ci: first implement

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ export ARCH=arm64
 export RSU_ENV=/rsuntk0
 export CLANG_VERSION=11
 export SHOW_TC_PATH=y
+export DATE=$(date +'%Y%m%d%H%M%S')
 
 # Print out function
 pr_info() {
@@ -39,10 +40,23 @@ elif [ ! -z $ENV_IS_CI ]; then
 	fi
 	pr_info "Environment is CI."
 	__CC=$ENV/clang-$CLANG_VERSION/bin/clang
-	__LD=$ENV/clang-$CLANG_VERSION/ld.lld
+	__LD=$ENV/clang-$CLANG_VERSION/bin/ld.lld
 	__CROSS_COMPILE=$ENV/google/bin/aarch64-linux-android-
 	__CLANG_TRIPLE=aarch64-linux-gnu-
 	__DEFCONFIG=$GIT_ENV_DEFCONFIG
+	
+	if [[ $KSU_STATE = 'true' ]]; then
+		FMT="RsuCI-`echo $DEVICE_VARIANT`-KSU_`echo $KSU_NUM`-`echo $SELINUX_STATE`"
+		curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/main/kernel/setup.sh" | bash -s main
+		
+		# KernelSU/kernel/Makefile#22
+		KSU_COMMIT_COUNT=$(cd KernelSU && git rev-list --count HEAD)
+		export KSU_VERSION=$(expr 10200 + $KSU_COMMIT_COUNT)
+	else
+		FMT="RsuCI-`echo $DEVICE_VARIANT`-`echo $SELINUX_STATE`"
+	fi
+	
+	echo $FMT > zipfile_format.txt
 else
 	# Fill it by yourself
 	__CC=
@@ -55,3 +69,14 @@ fi
 make --no-silent --jobs $(nproc --all) CC=$__CC LD=$__LD CROSS_COMPILE=$__CROSS_COMPILE CLANG_TRIPLE=$__CLANG_TRIPLE -C $(pwd) O=$(pwd)/out ARCH=arm64 `echo $__DEFCONFIG`
 export SHOW_TC_PATH=n
 make --no-silent --jobs $(nproc --all) CC=$__CC LD=$__LD CROSS_COMPILE=$__CROSS_COMPILE CLANG_TRIPLE=$__CLANG_TRIPLE -C $(pwd) O=$(pwd)/out ARCH=arm64
+
+if [ ! -z $ENV_IS_CI ]; then
+	cd Rissu
+	bash mk_version
+	cd AnyKernel3
+	zip -6 -r $(pwd)/Rissu/$FMT *
+	cd ../..
+fi
+if [ ! -z $ENV_IS_CI ] && [[ $UPLOAD_GZ = 'true' ]]; then
+	mv $(pwd)/out/arch/$ARCH/boot/Image.gz $(pwd)/Rissu/Image.gz
+fi
