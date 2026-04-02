0845091aaf75a0c9c64dd30b79c9eb6e4f077528
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-04-03 01:59:59 +0600

    Reapply "mm: make faultaround produce old ptes"
    On Android, application launch latency and direct reclaim time are reduced by
    10%+ and ~80% respectively [1] and this repository is targeted at Android. The
    revert was done because of a ~6% regression in unibench on x86, not Android.
    
    Do not handicap Android just to fix a x86 regression which won't be used at all
    on ARM64.
    
    This reverts commit 9775c770cd359a51ba830a489dcc33725623b47b.
    
    [1] https://github.com/torvalds/linux/commit/46bdb4277f98e70d0c91f4289897ade533fe9e80
    
    Change-Id: I8e0cfb8733d56213ab481ed5e6711533d1d89b9f
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -373,6 +373,7 @@ extern pgprot_t protection_map[16];
 #define FAULT_FLAG_REMOTE			0x80
 #define FAULT_FLAG_INSTRUCTION  		0x100
 #define FAULT_FLAG_INTERRUPTIBLE		0x200
+#define FAULT_FLAG_PREFAULT_OLD			0x400
 
 /*
  * The default fault flags that should be used by most of the
@@ -2947,6 +2948,7 @@ static inline void setup_nr_node_ids(void) {}
 #endif
 
 extern inline bool need_memory_boosting(void);
+extern int want_old_faultaround_pte;
 
 enum memsize_kernel_type {
 	MEMSIZE_KERNEL_KERNEL = 0,
