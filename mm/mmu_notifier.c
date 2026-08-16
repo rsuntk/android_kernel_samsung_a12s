2799e9c37d41fa7919d81b592d31eba4a1081f1f
rsuntk <rissu.ntk@gmail.com>
2026-08-16 17:33:26 +0700

    Revert "BACKPORT: mmap locking API: rename mmap_sem to mmap_lock"
    Reason for revert: "This breaks kernel ABI"
    This reverts commit edf418fd15bb7569cdf4a8206f8173607d837aa0.
    
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- mm/mmu_notifier.c
+++ mm/mmu_notifier.c
@@ -255,7 +255,7 @@ bool mm_has_blockable_invalidate_notifiers(struct mm_struct *mm)
 	int id;
 	bool ret = false;
 
-	WARN_ON_ONCE(!rwsem_is_locked(&mm->mmap_lock));
+	WARN_ON_ONCE(!rwsem_is_locked(&mm->mmap_sem));
 
 	if (!mm_has_notifiers(mm))
 		return ret;
