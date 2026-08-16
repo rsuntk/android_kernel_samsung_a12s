5c444d9120e03693447407e8d1516547cd36d0b6
rsuntk <rissu.ntk@gmail.com>
2026-08-16 17:33:26 +0700

    Revert "BACKPORT: mmap locking API: rename mmap_sem to mmap_lock"
    Reason for revert: "This breaks kernel ABI"
    This reverts commit edf418fd15bb7569cdf4a8206f8173607d837aa0.
    
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- fs/userfaultfd.c
+++ fs/userfaultfd.c
@@ -235,7 +235,7 @@ static inline bool userfaultfd_huge_must_wait(struct userfaultfd_ctx *ctx,
 	pte_t *ptep, pte;
 	bool ret = true;
 
-	VM_BUG_ON(!rwsem_is_locked(&mm->mmap_locked));
+	VM_BUG_ON(!rwsem_is_locked(&mm->mmap_sem));
 
 	ptep = huge_pte_offset(mm, address, vma_mmu_pagesize(vma));
 
@@ -287,7 +287,7 @@ static inline bool userfaultfd_must_wait(struct userfaultfd_ctx *ctx,
 	pte_t *pte;
 	bool ret = true;
 
-	VM_BUG_ON(!rwsem_is_locked(&mm->mmap_lock));
+	VM_BUG_ON(!rwsem_is_locked(&mm->mmap_sem));
 
 	pgd = pgd_offset(mm, address);
 	if (!pgd_present(*pgd))
@@ -401,7 +401,7 @@ vm_fault_t handle_userfault(struct vm_fault *vmf, unsigned long reason)
 	 * Coredumping runs without mmap_sem so we can only check that
 	 * the mmap_sem is held, if PF_DUMPCORE was not set.
 	 */
-	WARN_ON_ONCE(!rwsem_is_locked(&mm->mmap_lock));
+	WARN_ON_ONCE(!rwsem_is_locked(&mm->mmap_sem));
 
 	ctx = vmf->vma->vm_userfaultfd_ctx.ctx;
 	if (!ctx)
--- include/linux/mmap_lock.h
+++ include/linux/mmap_lock.h
@@ -3,58 +3,58 @@
 
 static inline void mmap_init_lock(struct mm_struct *mm)
 {
-	init_rwsem(&mm->mmap_lock);
+	init_rwsem(&mm->mmap_sem);
 }
 
 static inline void mmap_write_lock(struct mm_struct *mm)
 {
-	down_write(&mm->mmap_lock);
+	down_write(&mm->mmap_sem);
 }
 
 static inline int mmap_write_lock_killable(struct mm_struct *mm)
 {
-	return down_write_killable(&mm->mmap_lock);
+	return down_write_killable(&mm->mmap_sem);
 }
 
 static inline bool mmap_write_trylock(struct mm_struct *mm)
 {
-	return down_write_trylock(&mm->mmap_lock) != 0;
+	return down_write_trylock(&mm->mmap_sem) != 0;
 }
 
 static inline void mmap_write_unlock(struct mm_struct *mm)
 {
-	up_write(&mm->mmap_lock);
+	up_write(&mm->mmap_sem);
 }
 
 static inline void mmap_write_downgrade(struct mm_struct *mm)
 {
-	downgrade_write(&mm->mmap_lock);
+	downgrade_write(&mm->mmap_sem);
 }
 
 static inline void mmap_read_lock(struct mm_struct *mm)
 {
-	down_read(&mm->mmap_lock);
+	down_read(&mm->mmap_sem);
 }
 
 static inline int mmap_read_lock_killable(struct mm_struct *mm)
 {
-	return down_read_killable(&mm->mmap_lock);
+	return down_read_killable(&mm->mmap_sem);
 }
 
 static inline bool mmap_read_trylock(struct mm_struct *mm)
 {
-	return down_read_trylock(&mm->mmap_lock) != 0;
+	return down_read_trylock(&mm->mmap_sem) != 0;
 }
 
 static inline void mmap_read_unlock(struct mm_struct *mm)
 {
-	up_read(&mm->mmap_lock);
+	up_read(&mm->mmap_sem);
 }
 
 static inline bool mmap_read_trylock_non_owner(struct mm_struct *mm)
 {
-	if (down_read_trylock(&mm->mmap_lock)) {
-		rwsem_release(&mm->mmap_lock.dep_map, _RET_IP_);
+	if (down_read_trylock(&mm->mmap_sem)) {
+		rwsem_release(&mm->mmap_sem.dep_map, _RET_IP_);
 		return true;
 	}
 	return false;
@@ -62,12 +62,12 @@ static inline bool mmap_read_trylock_non_owner(struct mm_struct *mm)
 
 static inline void mmap_read_unlock_non_owner(struct mm_struct *mm)
 {
-	up_read_non_owner(&mm->mmap_lock);
+	up_read_non_owner(&mm->mmap_sem);
 }
 
 static inline int mmap_lock_is_contended(struct mm_struct *mm)
 {
-	return rwsem_is_contended(&mm->mmap_lock);
+	return rwsem_is_contended(&mm->mmap_sem);
 }
 
 #endif /* _LINUX_MMAP_LOCK_H */
